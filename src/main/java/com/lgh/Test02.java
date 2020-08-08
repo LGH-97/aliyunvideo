1e3f68685a87200ec7663016fa74254e5576627d
aliyunvideo <1327946548@qq.com>
2020-08-09 00:12:02 +0800

    111

--- src/main/java/com/lgh/Test02.java
+++ src/main/java/com/lgh/Test02.java
@@ -5,8 +5,8 @@ public class Test02 {
         System.out.println("hww888");
         System.out.println("hww111");
 
-        System.out.println("hww333");
         System.out.println("hww888");
+        System.out.println("hww333");
         System.out.println("hww888");
     }
 }
